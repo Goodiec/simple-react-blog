cf01db525b5dd7f6b30625203618c01ea2b1515f
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-07-12 12:50:31 +0100

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+    const {data:blogs, isLoading, error} = useFetch('http://localhost:5000/blogs');
+// const handleDelete = (id) =>{
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+// }
+
+
+    return ( 
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === "Mario")} title="Mario's Blogs"/> */}
+        </div>
+     );
+}
+ 
+export default Home;
